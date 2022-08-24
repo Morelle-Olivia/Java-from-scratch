134d39020f053ff3dd15cf3ab7e9af3266b2216d
Morelle-Olivia <hotoumorelle@gmail.com>
2022-08-24 09:27:21 +0100

    Implementing static and instance variables, static and instance methods, method overloading and method overiding in java.

---
+++ src/com/company/Animal.java
@@ -0,0 +1,114 @@
+package com.company;
+
+public class Animal {
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public void eat() {
+        System.out.println("Animal.eat() called");
+    }
+
+    public void move(int speed) {
+        System.out.println("Animal.move() called. Animal is moving at "+ speed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public void setBrain(int brain) {
+        this.brain = brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public void setBody(int body) {
+        this.body = body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
+
+class Dog extends Animal {
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog.chew() called.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called.");
+        chew();
+        super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called.");
+        move(5);
+    }
+
+    public void run() {
+        System.out.println("Dog.run() called.");
+        move(10);
+    }
+
+    private void moveLegs(int speed) {
+        System.out.println("Dog.moveLegs() called.");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called.");
+        moveLegs(speed);
+        super.move(speed);
+    }
+}
