e8ecb14ac43a663c74a9ad5c4921e4ca1eec488e
Morelle-Olivia <hotoumorelle@gmail.com>
2022-08-24 11:52:34 +0100

    completed my lessons for inheritance and did coding exercises.

---
+++ src/com/company/Vehicle.java
@@ -0,0 +1,52 @@
+package com.company;
+
+public class Vehicle {
+    private int steering;
+    private int gears;
+    private int speed;
+
+    public Vehicle(int steering, int gears, int speed) {
+        this.steering = steering;
+        this.gears = gears;
+        this.speed = speed;
+    }
+
+    public int getSteering() {
+        return steering;
+    }
+
+    public void setSteering(int steering) {
+        this.steering = steering;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
+class Cars extends Vehicle {
+    public Cars(int steering, int gears, int speed) {
+        super(steering, gears, speed);
+    }
+}
+
+class Yaris extends Cars {
+    private int spacious;
+
+    public Yaris(int steering, int gears, int speed, int spacious) {
+        super(steering, gears, speed);
+        this.spacious = spacious;
+    }
+}
--- src/com/company/module6Classes.java
+++ src/com/company/module6Classes.java
@@ -8,5 +8,14 @@ public class module6Classes {
         dog.eat();
         dog.walk();
         dog.run();
+
+        Circle circle = new Circle(3.75);
+        System.out.println("circle.radius= " + circle.getRadius());
+        System.out.println("circle.area= " + circle.getArea());
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder.radius= " + cylinder.getRadius());
+        System.out.println("cylinder.height= " + cylinder.getHeight());
+        System.out.println("cylinder.area= " + cylinder.getArea());
+        System.out.println("cylinder.volume= " + cylinder.getVolume());
     }
 }
